d58b0eceb95893c1a73fe681df2896e8747138a8
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2023-02-02 08:33:23 -0600

    Port

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const api = require('./routes/index.js');
 
 const app = express();
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
